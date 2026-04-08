f6e7f05fad09ca51b4fb9e81a6ffea37b3d52ef3
Shayan Malik <shayaaanmalik@gmail.com>
2026-04-08 16:57:52 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-fetch(`https://dummyjson.com/products`)
+fetch("https://kolzsticks.github.io/Free-Ecommerce-Products-Api/main/products.json")
 .then(function(response){
     return response.json()
 })
 .then(function(data){
     console.log(data);
 
+    let allCards = "";
+
     for(let i = 0; i < data.length; i++){
-        document.getElementById("cards").innerHTML +=
-        `<div class="card">
-            <div class="card-div">
-                <img src="${data[i].image}" alt="">
-            </div>
+        allCards += `<div class="card">
+                <div class="card-div">
+                    <img src="${data[i].image}" alt="">
+                </div>
 
-            <div class="price">
-                <p>${data[i].title}</p>
-                <h3>$${data[i].price}</h3>
-            </div>
+                <div class="price">
+                    <p>${data[i].name}</p>
+                    <h3>$${data[i].priceCents}</h3>
+                </div>
 
-
-        </div>`
+            </div>`
     }
-    
+
+    document.getElementById("cards").innerHTML = allCards
     
 })
+.catch(function(error){
+    console.log(error);
+    
+})
+
+
+
+// const getApiData = async() => {
+//     try{
+//         const data = await fetch("https://kolzsticks.github.io/Free-Ecommerce-Products-Api/main/products.json");
+//         const jsonData = await data.json();
+//         let allCards = "";
+
+//         for(let i = 0; i < data.length; i++){
+//         allCards += `<div class="card">
+//                 <div class="card-div">
+//                     <img src="${data[i].image}" alt="">
+//                 </div>
+
+//                 <div class="price">
+//                     <p>${data[i].name}</p>
+//                     <h3>$${data[i].priceCents}</h3>
+//                 </div>
+
+//             </div>`
+//     }
+
+        
+
+//         document.getElementById("cards").innerHTML = allCards;
+
+//     }
+//     catch(error){
+//         console.log(error);
+        
+//     }
+// };
+
+// getApiData();
+
+// console.log("test");
+
+
+
